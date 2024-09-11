cecc87bf1c394de411726e64f9ea335625e7a0cd
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-09-11 21:30:13 +0800

    completed variables section

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+let name = "Rona Mae A. Ancheta";
+console.log('My name is ' + name);
+
+let want_do_you_wannabe_come_in_your_life= 'programmer';
+console.log(want_do_you_wannabe_come_in_your_life);
+
+let gender = "Female";
+console.log("gender: " + gender);
+
+let Twitter_handle = 'ronamae@gmail.com';
+console.log(Twitter_handle);
